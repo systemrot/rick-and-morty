d11c3ea86c8074c09f12bc9f9b62505a8a2818cd
r.dyachenko <newxpyct@gmail.com>
2021-08-17 16:00:00 +0300

    fix pagination

--- src/view/CharacterList/index.tsx
+++ src/view/CharacterList/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 import { useGetCharacters } from '../../api/hooks/characters/useGetCharacters';
 import { Character } from '../../components/Character';
 import { Pagination } from '../../components/Pagination';
@@ -11,6 +12,10 @@ interface IProps {
 export const CharacterList: React.FC<IProps> = ({ value }) => {
   const [paginationUrl, setPaginationUrl] = useState<string | null>(null);
   const { data, loading, error } = useGetCharacters(value, paginationUrl);
+
+  useEffect(() => {
+    setPaginationUrl(null);
+  }, [value]);
 
   if (loading) {
     return <p>Данные загружаются...</p>;
